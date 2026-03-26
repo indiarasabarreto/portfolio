c88f2a60ce1a003196929f0250397cd27e19e5a1
indiarasabarreto <indiara86@gmail.com>
2026-03-26 07:42:45 -0300

    "data_analysis"

--- analise-dados/analized_data.py
+++ analise-dados/analized_data.py
@@ -2,11 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('coffee_data.csv')
+print(df.head(5))
 
 df = df.dropna()
 
-month_sales = df.groupby('Month')['Sales'].sum()
+month_sales = df.groupby(["date"])["money"].sum()
 
 month_sales.plot(kind='bar')
 plt.title('Total Sales by Month')
-plt.show()
+plt.show() 
+
