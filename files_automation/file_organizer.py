b099ec89feb49d4cbc482bb8d3ee8a0623706500
indiarasabarreto <indiara86@gmail.com>
2026-04-07 05:51:33 -0300

    updated

--- files_automation/file_organizer.py
+++ files_automation/file_organizer.py
@@ -1,8 +1,14 @@
 import os
 import shutil
 
-file_organizer = 'files'
-destination_folder = 'organized_files'
+
+base_directory = os.path.dirname(os.path.abspath(__file__))
+
+
+file_organizer = os.path.join(base_directory, 'organized_files')
+destination_folder = os.path.join(base_directory, 'files')
+
+
 
 extensions = {
     'images': ['.WebP', '.png'],
@@ -10,12 +16,23 @@ extensions = {
     'spreadsheets': ['.xlsx']
 }
 
+
+if not os.path.exists(file_organizer):
+    print(f'Error: The directory "{file_organizer}" does not exist.')
+    exit()
+
 for filename in os.listdir(file_organizer):
     file_path = os.path.join(file_organizer, filename)
 
-    for paste, types in extensions.items():
-        if any(filename.endswith(ext) for ext in types):
-            destination_path = os.path.join(destination_folder, paste)
-            os.makedirs(destination_path, exist_ok=True)
-            shutil.move(file_path, os.path.join(destination_path, filename))
-            break
+    if os.path.isfile(file_path):
+        name, extension = os.path.splitext(filename)
+
+
+        for paste, types in extensions.items():
+            if extension.lower() in types:
+                destination = os.path.join(destination_folder, paste)
+                os.makedirs(destination, exist_ok=True)
+
+                shutil.move(file_path, os.path.join(destination, filename))
+                print(f'Moved: {filename} to {destination}')
+                break
