bda48486b1db9aef8b0004f147a186901af766d2
indiarasabarreto <indiara86@gmail.com>
2026-04-07 10:49:05 -0300

    code fixed

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,14 +2,12 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('coffee_data.csv')
+print(df.columns)
 
 st.title("Dashboard de Vendas")
 
-product = st.selectbox("Escolha o produto", df['coffee_name'].unique())
-
-filtered_data = df[df[df['product'] == product]]
-
+coffee_name = st.selectbox("Escolha o café", df['coffee_name'].unique())
+filtered_data = df[df['coffee_name'] == coffee_name]
 st.write(filtered_data)
-
 st.bar_chart(filtered_data.groupby('date')['money'].sum())
 
